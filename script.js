ad1c258003747f4325db0eb89dbfffc21fb8420d
ricorodriguez6596-boop <ricorodriguez6596@gmail.com>
2026-07-22 09:48:41 +0530

    New files has been added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let hamMenu = document.querySelector('.ham-menu-button')
 let navList = document.querySelector('.nav-list')
 let section = document.querySelector('.news-grid-section')
-let hamOpenImg=document.querySelector('.ham-open-img')
-let hamCloseImg=document.querySelector('.ham-close-img')
+let hamOpenImg = document.querySelector('.ham-open-img')
+let hamCloseImg = document.querySelector('.ham-close-img')
 
 let menuOpen = true
 
@@ -12,21 +12,21 @@ let expand = () => {
         navList.style.right = 0
         navList.style.opacity = 1
         menuOpen = false
-        hamOpenImg.style.display='none'
-        hamCloseImg.style.display='block'
+        hamOpenImg.style.display = 'none'
+        hamCloseImg.style.display = 'block'
     } else {
         navList.style.right = ''
         navList.style.opacity = 0
         menuOpen = true
-        hamOpenImg.style.display='block'
-        hamCloseImg.style.display='none'
+        hamOpenImg.style.display = 'block'
+        hamCloseImg.style.display = 'none'
     }
 
     let clos = () => {
         if (menuOpen === false) {
             navList.style.opacity = 0
-             hamOpenImg.style.display='block'
-        hamCloseImg.style.display='none'
+            hamOpenImg.style.display = 'block'
+            hamCloseImg.style.display = 'none'
             menuOpen = true
         }
     }
